0492430870698f8ddcaebcbc7598cad8a9cc1061
meishild <meishild@gmail.com>
2023-03-27 23:00:49 +0800

    add installer、tensorboard

--- run_popup.bat
+++ run_popup.bat
@@ -2,7 +2,8 @@
 
 echo Starting popup training...
 call sd_scripts\venv\Scripts\activate
+python.exe -m pip install --upgrade pip
 echo updating LyCORIS if needed...
-pip install LyCORIS\. > nul
+pip install LyCORIS\. 
 python main.py --popup
 pause
